0ddabe89649cd009d3b89b9d0fcf9e30c54dce29
Tusharisme <23f2003751@ds.study.iitm.ac.in>
2025-03-31 20:46:34 +0530

    second commit

--- process_yt.py
+++ process_yt.py
@@ -9,7 +9,8 @@ def get_transcript(question):
     file_path = os.path.join(os.path.dirname(__file__), "transcript.xlsx")
     df = pd.read_excel(file_path)
     match = re.search(
-        r'between (\d+(?:\.\d+)?) and (\d+(?:\.\d+)?) seconds?', question, re.IGNORECASE)
+        r"between (\d+(?:\.\d+)?) and (\d+(?:\.\d+)?) seconds?", question, re.IGNORECASE
+    )
     if not match:
         raise ValueError("Could not extract time range from the question.")
     try:
@@ -20,29 +21,35 @@ def get_transcript(question):
 
     transcript = ""
     for index, row in df.iterrows():
-        if row['timestamp'] >= start_time and row['timestamp'] <= end_time:
-            transcript += str(row['text'])+" "
+        if row["timestamp"] >= start_time and row["timestamp"] <= end_time:
+            transcript += str(row["text"]) + " "
     # print(transcript)
     return transcript
 
 
-API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjIwMDE2NDArMUBkcy5zdHVkeS5paXRtLmFjLmluIn0.Oeg6lAaRenn3gnBWd6qaGscvatJ6ftTpvw-waESMVs8"
+API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDM3NTFAZHMuc3R1ZHkuaWl0bS5hYy5pbiJ9.twnougmakpXTfQUGrtiBUc2WSRe8LAROPEuPSC3RXSw"
 
 
 def correct_transcript(transcript):
     BASE_URL = "https://aiproxy.sanand.workers.dev/openai/v1"
     data = {
         "model": "gpt-4o-mini",
-        "messages": [{"role": "user", "content": "correct this transcript: "+transcript+" return only the answer and nothing else , not extra text"}]
+        "messages": [
+            {
+                "role": "user",
+                "content": "correct this transcript: "
+                + transcript
+                + " return only the answer and nothing else , not extra text",
+            }
+        ],
     }
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {API_KEY}"
-    }
-    response = httpx.post(BASE_URL + "/chat/completions",
-                          json=data, headers=headers, timeout=60)
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_KEY}"}
+    response = httpx.post(
+        BASE_URL + "/chat/completions", json=data, headers=headers, timeout=60
+    )
 
     return response.json().get("choices", [])[0].get("message", {}).get("content")
+
 
 # question = "What is the text of the transcript of this Mystery Story Audiobook between 404.9 and 478.3 seconds?"
 # print(correct_transcript(get_transcript(question)))
